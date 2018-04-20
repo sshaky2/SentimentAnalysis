60d9f96335fca9250d2f4b61b827c35eca1593de
Shobhit Shakya <sshakya@korewireless.com>
2018-04-20 15:31:20 -0400

    message editted

--- sentiment.py
+++ sentiment.py
@@ -13,7 +13,9 @@ app = Flask(__name__)
 def get_score():
     text = request.data.decode(encoding='UTF-8')
     if (not text):
-        return '<h1>In order to calculate the sentiment score, you need to HTTP POST with text in body to this address.</h1>'
+        return '<h1>In order to calculate the sentiment score, you need to HTTP POST the body with text to this address.</h1><br> \
+        Score is return in form of JSON object. Compound score is the average score which ranges from -1 to +1. -1 being most negative and \
+        +1 being most positive.'
     else:
         sid = SentimentIntensityAnalyzer()
         return jsonify(sid.polarity_scores(request.data.decode(encoding='UTF-8')))
