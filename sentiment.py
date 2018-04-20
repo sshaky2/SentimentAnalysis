fd439e0010ffd92a62f4c547eb1e55b564014d0e
Shobhit Shakya <sshakya@korewireless.com>
2018-04-20 11:47:52 -0400

    change

--- sentiment.py
+++ sentiment.py
@@ -4,15 +4,15 @@ from flask import Flask, jsonify, request
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-#nltk.download('punkt')
-#nltk.download('vader_lexicon')
+nltk.download('punkt')
+nltk.download('vader_lexicon')
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return 'Hello World!'
+    return "Hello World!"
 
 
 if __name__ == '__main__':
