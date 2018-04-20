5934d74a99627202c5c63b8826a1cbc7678af748
Shobhit Shakya <sshakya@korewireless.com>
2018-04-20 14:59:58 -0400

    get message added

--- sentiment.py
+++ sentiment.py
@@ -9,10 +9,14 @@ nltk.download('vader_lexicon')
 app = Flask(__name__)
 
 
-@app.route("/", methods=['POST'])
+@app.route("/", methods=['GET', 'POST'])
 def get_score():
-    sid = SentimentIntensityAnalyzer()
-    return jsonify(sid.polarity_scores(request.data.decode(encoding='UTF-8')))
+    text = request.data.decode(encoding='UTF-8')
+    if (not text):
+        return '<h1>In order to calculate the sentiment score, you need to HTTP POST with text in body to this address.</h1>'
+    else:
+        sid = SentimentIntensityAnalyzer()
+        return jsonify(sid.polarity_scores(request.data.decode(encoding='UTF-8')))
 
 
 if __name__ == '__main__':
